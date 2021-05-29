75b96f18e0a396dc8124179ee9dcf8789856cae1
Vladislav Ristovski <vaci.ristovski@gmail.com>
2021-05-30 01:49:39 +0200

    Initial commit

---
+++ src/test/java/SILab2Test.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SILab2Test {
+    private final Time time = new Time();
+
+    @Test
+    void assertationTest(){
+        Assert.assertEquals();
+
+    }
+}
